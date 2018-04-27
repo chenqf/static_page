e27fcf6ba5ccd767b58acc0cf5bdad05c50fdada
chenqf <chenqfboss@gmail.com>
2018-04-27 11:22:21 +0800

    flex

---
+++ file.list.js
@@ -0,0 +1,19 @@
+// @flow Created by 陈其丰 on 2018/4/26.
+
+
+export default{
+    jd:[
+        {
+            key:'mine',
+            name:'我的'
+        },
+        {
+            key:'main',
+            name:'首页'
+        },
+        {
+            key:'account',
+            name:'账号管理'
+        }
+    ]
+}
