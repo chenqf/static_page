38c193cc993c6f9db6a727c273956c61d0f23383
chenqf <qifeng.chen1006@foxmail.com>
2018-05-21 16:04:19 +0800

    验证 react portal

---
+++ apps/demo/portal/index.js
@@ -0,0 +1,67 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import 'style'
+import './index.scss'
+
+
+
+class C extends Component{
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+    render(){
+        return ReactDOM.createPortal(
+            <div>123</div>,
+            document.getElementById('root1')
+        )
+    }
+}
+
+function C2(props){
+    return (
+        <div/>
+    );
+}
+
+
+ReactDOM.render(
+    <C/>
+    ,
+    document.getElementById('root')
+);
+
+
+class dd extends Component{
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+    render(){
+        return <div/>
+    }
+}
+
+
+class CC extends Component{
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+    render(){
+        return <div/>
+    }
+}
+
+// ReactDOM.render(
+//     <div/>
+//     ,
+//     document.getElementById('root')
+// );
+
+// ReactDOM.render(
+//     <div/>
+//     ,
+//     document.getElementById('root')
+// );
