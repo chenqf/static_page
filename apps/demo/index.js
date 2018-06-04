8695f6effb5ab799a47e705960adb1ebd4e34631
chenqf <qifeng.chen1006@foxmail.com>
2018-06-04 15:12:49 +0800

    use strict

--- apps/demo/index.js
+++ apps/demo/index.js
@@ -1,22 +1,57 @@
 
+console.log(070 === 56);
+
+// function Test(a,b,c){
+//     this.a = a;
+//     this.b = b;
+//     this.c = c;
+// }
+//
+// Test.prototype.name = 'test bind';
+//
+// Test.prototype.log = function () {
+//     console.log(this.constructor === Test); // true
+//     console.log(this.name); // test bind
+//     console.log(this.a,this.b,this.c); // 1 2 3
+// }
+//
+// const Demo = Test.bind(null,1,2);
+//
+// let ins = new Demo(3);
+// ins.log();
+// console.log(Demo.length); // 1
 
 
-function Test(a,b,c){
-    this.a = a;
-    this.b = b;
-    this.c = c;
+let n = new Number(100);
+console.log(n instanceof Number);    //true
+
+let num = 10;
+console.log(num instanceof Number);  //false
+
+
+function test() {
+    eval('var a = 1;');
+    console.log(typeof a); // 正常模式： number
 }
 
-Test.prototype.name = 'test bind';
-
-Test.prototype.log = function () {
-    console.log(this.constructor === Test); // true
-    console.log(this.name); // test bind
-    console.log(this.a,this.b,this.c); // 1 2 3
+function test() {
+    "use strict";
+    eval('var a = 1;');
+    console.log(typeof a); // 严格模式： undefined
 }
 
-const Demo = Test.bind(null,1,2);
+function test() {
+    "use strict";
+    return (function () {
+        return this !== undefined;
+    })();
+}
 
-let ins = new Demo(3);
-ins.log();
-console.log(Demo.length); // 1
+(function () {
+    return this === undefined; // 严格模式：true  正常模式：false
+})();
+
+
+
+
+
